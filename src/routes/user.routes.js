6ae7c66c52f12771d7fab62efba81bfdf61eab80
sbmray <shubhamray54321@gmail.com>
2025-08-04 12:35:49 +0530

    add register controller and bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar", // Assuming 'avatar' is the field name for the image upload
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1, // ek hi image upload kar sakte hai
+    },
+  ]),
+  registerUser
+);
 
 export default router;
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,24 +1,29 @@
 class ApiError extends Error {
-    constructor(
-        statusCode,
-        message= "Something went wrong",
-        errors = [],
-        stack = ""
-    ){
-        super(message)
-        this.statusCode = statusCode
-        this.data = null
-        this.message = message
-        this.success = false;
-        this.errors = errors
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
 
-        if (stack) {
-            this.stack = stack
-        } else{
-            Error.captureStackTrace(this, this.constructor)
-        }
+    console.log("ApiError:", {
+      statusCode,
+      message,
+      errors,
+    });
 
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
+  }
 }
 
-export {ApiError}
+export { ApiError };
